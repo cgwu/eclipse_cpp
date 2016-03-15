45d7c71a6c2d4b9cb47a4aa4fccb496d677894ca
cgwu <gg.wcg@qq.com>
2016-03-16 00:09:00 +0800

    WPF test.

---
+++ WPF_Demos/FirstWpfApp/FirstWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("button clicked:" + (sender as Button).Content);
+            //MessageBox.Show("Hello WPF, 我喜欢你!");
+            //MessageBox.Show(string.Format("The grid is {0}x{1} units in size.", 
+            //    grid1.ActualWidth, grid1.ActualHeight));
+        }
+    }
+}
