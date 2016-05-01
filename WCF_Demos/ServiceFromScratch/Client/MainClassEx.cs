155a5ab2d3e482ce6a06182b3ca534ec4e3d7fe2
gg <gg.wcg@qq.com>
2016-05-01 14:05:36 +0800

    测试成功完成。

--- WCF_Demos/ServiceFromScratch/Client/MainClassEx.cs
+++ WCF_Demos/ServiceFromScratch/Client/MainClassEx.cs
@@ -11,9 +11,9 @@ namespace Client
         {
             HelloIndigoExService.HelloIndigoExServiceClient client =
                 /* 使用basicHttpBinding */
-                //new HelloIndigoExService.HelloIndigoExServiceClient("BasicHttpBinding_IHelloIndigoExService");
+                new HelloIndigoExService.HelloIndigoExServiceClient("BasicHttpBinding_IHelloIndigoExService");
                 /* 使用netTcpBinding */
-                new HelloIndigoExService.HelloIndigoExServiceClient("NetTcpBinding_IHelloIndigoExService");
+                //new HelloIndigoExService.HelloIndigoExServiceClient("NetTcpBinding_IHelloIndigoExService");
             string s = client.GetMessage("吴xx");
             Console.WriteLine(s);
             Console.WriteLine("Press <ENTER> to terminate Client.");
