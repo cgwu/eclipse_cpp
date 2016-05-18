87e5db25174a66a99bc28db237e52c8256843fb6
gg <gg.wcg@qq.com>
2016-05-18 13:03:33 +0800

    Msmq Test OK.

---
+++ WCF_Demos/MyMsmqSender/MyMsmqReceiver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+
+namespace MyMsmqReceiver
+{
+    class Program
+    {
+
+        const string QueuePath = @".\Private$\MyQueue";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latest message");
+            MessageQueue myQueue;
+            myQueue = new MessageQueue(QueuePath);
+            Message msg = myQueue.Receive();
+            msg.Formatter = new BinaryMessageFormatter();
+            Console.WriteLine(msg.Body.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCF_Demos/MyMsmqSender/MyMsmqSender/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+
+namespace MyMsmqSender
+{
+    class Program
+    {
+        const string QueuePath = @".\Private$\MyQueue";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Message to be sent");
+            Console.WriteLine("(High priority messages should start with 'HP:'");
+            string msgToBeSent = Console.ReadLine();
+
+            // Get or Create Queue
+            MessageQueue myQueue;
+            if (MessageQueue.Exists(QueuePath))
+            {
+                myQueue = new MessageQueue(QueuePath);
+            }
+            else
+            {
+                myQueue = MessageQueue.Create(QueuePath);
+            }
+
+            // Create Message
+            Message msg = new Message();
+            // Set Formatter for Message
+            msg.Formatter = new BinaryMessageFormatter();
+            msg.Body = msgToBeSent;
+            msg.Label = "App1Message";
+
+            if (msgToBeSent.StartsWith("HP:"))
+            {
+                msg.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                msg.Priority = MessagePriority.Normal;
+            }
+            myQueue.Send(msg);
+            Console.WriteLine("Thanks for sending message");
+            Console.ReadKey();
+        }
+    }
+}
